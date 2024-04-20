9b95cb94c58da4a0a626f0c8a0c4df394b62c000
Alejandro Martínez Valarino <94855443+amarval387@users.noreply.github.com>
2024-04-21 00:27:10 +0200

    vercel url 2

--- DjangoMangaPlus/urls.py
+++ DjangoMangaPlus/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mangaplus_app.urls', namespace='mangaplus_app')),
+    path('', include('mangaplus_app.urls', namespace='')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

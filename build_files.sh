2c16c02d7951a14525f953d54b457f0dc89b9074
Alejandro Martínez Valarino <94855443+amarval387@users.noreply.github.com>
2024-04-21 00:13:56 +0200

    vercel build 7

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Creating virtual environment..."
-python3.9 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
